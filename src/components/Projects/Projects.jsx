b21c057a35879c1294050a2faff871015d3475dc
Chris Kildunne <136514462+kilzridesbikes@users.noreply.github.com>
2023-10-04 15:50:26 -0600

    Projects Page Styling

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,10 +3,16 @@ import './Projects.css';
 export default function Projects() {
     return (
         <div className="projects-wrapper">
-         
+            
+            {/* Board-Shop Project */}
             <div className="card board-container shadow-lg board-shop-card">
                 <div className="card-body">
-                    <div className="card-text">
+                    <ul className="project-details">
+                        <li>Engineered a full-stack, e-commerce snowboard shop, using React.js and Node.js, integrating full CRUD operations. </li>
+                        <li>Incorporated Stripe API for secure processing of one-time online payments. </li>
+                        <li> Leveraged Bootstrap for UI/UX design, utilizing pre-built components, to accelerate development.</li>
+                    </ul>
+                    <div className="card-links">
                         <a
                             className="github-project"
                             href="https://github.com/ChrisKildunne/board-shop"
@@ -15,8 +21,7 @@ export default function Projects() {
                         >
                             Board-Shop Code
                         </a>
-                    </div>
-                    <div className="card-text">
+                        &nbsp; || &nbsp;
                         <a
                             className="github-project"
                             href="https://board-shop-81f8a4bd2226.herokuapp.com/orders/new"
@@ -29,9 +34,16 @@ export default function Projects() {
                 </div>
             </div>
 
+            {/* Outings Project */}
             <div className="card board-container shadow-lg outings-card">
                 <div className="card-body">
-                    <div className="card-text">
+                    <ul className="project-details">
+                        <li>Utilized PostgreSQL, Django, and Python to build a full-stack application from scratch, with full CRUD operations. </li>
+                        <li>Leveraged Django's built-in authentication system, which provided a robust and reliable framework for user management. This system allowed my team to integrate user authentication features into the application. </li>
+                        <li>Collaborated with a team of developers to build a website where party planners can post events that they are hosting surrounding sporting events, i.e tailgates, after parties etc.</li>
+                        <li>Integrated Google Maps API to enable users to seamlessly post event locations.</li>
+                    </ul>
+                    <div className="card-links">
                         <a
                             className="github-project"
                             href="https://github.com/ChrisKildunne/outings"
@@ -40,8 +52,7 @@ export default function Projects() {
                         >
                             Outings Code
                         </a>
-                    </div>
-                    <div className="card-text">
+                        &nbsp; || &nbsp;
                         <a
                             className="github-project"
                             href="https://outings-af5fe22770a5.herokuapp.com/"
@@ -53,6 +64,7 @@ export default function Projects() {
                     </div>
                 </div>
             </div>
+
         </div>
     );
 }
