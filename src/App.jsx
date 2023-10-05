b7780f05896eebde7768d1518f6ccab03a6ee8c4
Chris Kildunne <136514462+kilzridesbikes@users.noreply.github.com>
2023-10-05 16:27:36 -0600

    styling finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,34 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Header from './components/Header/Header';
 import Projects from './components/Projects/Projects';
 import AboutMe from './components/AboutMe/AboutMe';
 import Home from './components/Home/Home';
 
+function AnimatedRoutes() {
+    let location = useLocation();
+
+    return (
+        <TransitionGroup>
+            <CSSTransition key={location.key} classNames="fade" timeout={300}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/about" element={<AboutMe />} />
+                </Routes>
+            </CSSTransition>
+        </TransitionGroup>
+    );
+}
+
 export default function App() {
-  return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<AboutMe />} />
-        </Routes>
-      </Router>
-    </>
-  );
+    return (
+        <Router>
+            <Header />
+            <AnimatedRoutes />
+        </Router>
+    );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Home.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default function Home() {
-    const [nameDisplay, setNameDisplay] = useState('');
-    const fullName = "C-hris Kildunne";
-
-    useEffect(() => {
-        let i = 0;
-        const timer = setInterval(() => {
-            if (i < fullName.length) {
-                setNameDisplay((prevName) => prevName + fullName.charAt(i));
-                i++;
-            } else {
-                clearInterval(timer);
-            }
-        }, 100);
-
-        return () => clearInterval(timer);
-    }, []);
-
     const scrollToContact = () => {
         window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' }); 
     }
@@ -29,11 +12,9 @@ export default function Home() {
         <div>
             <div className="container mt-5 text-center">
                 <img src="https://i.imgur.com/UAXl5cF.jpg" alt="Chris Kildunne" className="img-fluid rounded-circle" style={{ maxWidth: "200px" }} />
-                <h1 className="mt-3" style={{ fontFamily: '"Lucida Console", Monaco, monospace', letterSpacing: '2px' }}>
-                    {nameDisplay.split("").map((letter, index) => (
-                        <span key={index} style={{ animation: 'drop 0.3s forwards', animationDelay: `${index * 100}ms` }}>{letter}</span>
-                    ))}
-                </h1>
+                <div className="typewriter">
+                  <h1>Chris Kildunne</h1>
+                </div>
                 <h3 style={{ fontStyle: 'italic', color: '#555' }}>
                     Software Engineer
                 </h3>
@@ -41,28 +22,27 @@ export default function Home() {
             </div>
             
             <div id="contact-section">
-              <h2>Contact</h2>
-              <p>
-                  <strong>Email:</strong> <a href="mailto:chriskildunne@gmail.com" className="contact-link">chriskildunne@gmail.com</a>
-              </p>
-              <p><strong>Phone:</strong> 516-404-9876</p>
-              <a className="nav-link"
-                  href="https://github.com/ChrisKildunne"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faGithub} size="2x" />
-              </a>
+                <h2>Contact</h2>
+                <p>
+                    <strong>Email:</strong> <a href="mailto:chriskildunne@gmail.com" className="contact-link">chriskildunne@gmail.com</a>
+                </p>
+                <p><strong>Phone:</strong> 516-404-9876</p>
+                <a className="nav-link"
+                   href="https://github.com/ChrisKildunne"
+                   target="_blank"
+                   rel="noopener noreferrer">
+                   <FontAwesomeIcon icon={faGithub} size="2x" />
+                </a>
 
-              <span className="navbar-text mx-3"></span>
+                <span className="navbar-text mx-3"></span>
 
-              <a className="nav-link"
-                  href="https://www.linkedin.com/in/christopher-kildunne/"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faLinkedin} size="2x" />
-              </a>
-          </div>
-
+                <a className="nav-link"
+                   href="https://www.linkedin.com/in/christopher-kildunne/"
+                   target="_blank"
+                   rel="noopener noreferrer">
+                   <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                </a>
+            </div>
         </div>
     );
 }
